414ac451a33d1f60030ea626b3cbf04b134181df
sodas <sodas2002@gmail.com>
2011-03-11 14:04:09 +0800

    Change to Django 1.2

--- TaiwanWeatherGAE/src/MainHandler.py
+++ TaiwanWeatherGAE/src/MainHandler.py
@@ -1,4 +1,8 @@
 # coding=utf8
+# Django 1.2
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
--- TaiwanWeatherGAE/src/parser/ForecastHandler.py
+++ TaiwanWeatherGAE/src/parser/ForecastHandler.py
@@ -1,4 +1,8 @@
 # coding=utf8
+# Django 1.2
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import urlfetch, memcache
