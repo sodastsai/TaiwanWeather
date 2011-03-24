d264708d2da6980a8fb5ac460722ef3a2c972914
sodas <sodas2002@gmail.com>
2011-03-24 16:59:00 +0800

    Fetch city list from web

--- TaiwanWeatherWP7/TaiwanWeatherWP7/MainPage.xaml.cs
+++ TaiwanWeatherWP7/TaiwanWeatherWP7/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,9 +11,21 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Text;
 
 namespace TaiwanWeatherWP7 {
+    // City list object
+    public class City {
+        public string name {set; get;}
+        public string enName {set; get;}
+    }
+
     public partial class MainPage : PhoneApplicationPage {
+        // Data source
+        List<City> cityLists;
+
         // Constructor
         public MainPage() {
             InitializeComponent();
@@ -24,22 +36,40 @@ namespace TaiwanWeatherWP7 {
         }
 
         // Handle selection changed on ListBox
-        private void MainListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        private void CityListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             // If selected index is -1 (no selection) do nothing
-            if (MainListBox.SelectedIndex == -1)
+            if (CityListBox.SelectedIndex == -1)
                 return;
 
             // Navigate to the new page
-            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + MainListBox.SelectedIndex, UriKind.Relative));
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + CityListBox.SelectedIndex, UriKind.Relative));
 
             // Reset selected index to -1 (no selection)
-            MainListBox.SelectedIndex = -1;
+            CityListBox.SelectedIndex = -1;
         }
 
         // Load data for the ViewModel Items
         private void MainPage_Loaded(object sender, RoutedEventArgs e) {
             if (!App.ViewModel.IsDataLoaded) {
                 App.ViewModel.LoadData();
+
+                // Get a web client to fetch string from Network
+                var webClient = new WebClient();
+                webClient.OpenReadAsync(new Uri("http://ntu-taiwan-weather.appspot.com/json/city/"));
+                webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(webClientCompletedRead); // Completed Method
+            }
+        }
+
+        void webClientCompletedRead(object sender, OpenReadCompletedEventArgs e) {
+            using (var reader = new StreamReader(e.Result)) {
+                // Get string
+                string result = reader.ReadToEnd();
+                // Convert from JSON to Object
+                MemoryStream jsonStream = new MemoryStream(Encoding.Unicode.GetBytes(result));
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<City>));
+                cityLists = (List<City>)serializer.ReadObject(jsonStream);
+                // Make it to city list box
+                CityListBox.ItemsSource = cityLists;
             }
         }
 
@@ -47,12 +77,16 @@ namespace TaiwanWeatherWP7 {
         private void CWB_Pressed(object sender, EventArgs e) {
             WebBrowserTask cwbWeb = new WebBrowserTask();
             cwbWeb.URL = "http://www.cwb.gov.tw/";
-               cwbWeb.Show();
+            cwbWeb.Show();
         }
 
         // Show lab's page
         private void AboutUs_Pressed(object sender, EventArgs e) {
-            NavigationService.Navigate(new Uri("/Views/AboutUs.xaml", UriKind.Relative));
+            NavigationService.Navigate(new Uri("AboutUs.xaml", UriKind.Relative));
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e) {
+
         }
     }
 }
